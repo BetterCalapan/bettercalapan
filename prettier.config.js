ecc1761d84a093710fc01d92a622f5160931e2fe
Arvin Garcia <arvin@arvingarcia.com>
2026-08-05 21:18:25 +0800

    chore(formatting): change to double quotes

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
 	useTabs: true,
-	singleQuote: true,
-	trailingComma: 'none',
+	singleQuote: false,
+	trailingComma: "none",
 	printWidth: 100,
-	plugins: ['prettier-plugin-svelte'],
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+	plugins: ["prettier-plugin-svelte"],
+	overrides: [{ files: "*.svelte", options: { parser: "svelte" } }]
 };
 
 export default config;
